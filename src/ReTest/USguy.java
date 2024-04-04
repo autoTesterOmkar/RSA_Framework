8ad77a52f31338459d182783b77b22cc7648336b
Omkar <opawar449@gmail.com>
2024-04-04 17:13:58 +0530

    commit from Indian Guy

--- src/ReTest/USguy.java
+++ src/ReTest/USguy.java
@@ -8,5 +8,10 @@ public class USguy {
 	public void Demo() {
 		System.out.println("Im US guy and pull code and do changes then push again");
 	}
+	
+	@Test
+	public void Demo1() {
+		System.out.println("Im Indian guy and Nice to meet you");
+	}
 
 }
