cb816266ee77782cf2087b552284bc08bb03c295
Omkar <opawar449@gmail.com>
2024-04-04 16:56:09 +0530

    commit from US guy

---
+++ src/ReTest/USguy.java
@@ -0,0 +1,12 @@
+package ReTest;
+
+import org.testng.annotations.Test;
+
+public class USguy {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Im US guy and pull code and do changes then push again");
+	}
+
+}
